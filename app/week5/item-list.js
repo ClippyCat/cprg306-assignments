3e2482df1fe669df6d9f8c983aa4a115b9581882
melody580085 <melody580085@gmail.com>
2023-10-07 15:48:34 -0600

    fix json

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -1,11 +1,11 @@
 "use client";
 import React, { useState } from 'react';
-import itemsData from './items.json'; // Import the items from the JSON file.
+import Item from './items';
+import itemsData from './items.json';
 
-function ItemList() {
-  // Initialize state variables
+const ItemList = () => {
   const [sortBy, setSortBy] = useState('name');
-  const [grouped, setGrouped] = useState(false); // Added state for grouping
+  const [grouped, setGrouped] = useState(false);
 
   // Sort the items based on the sortBy state variable
   const sortedItems = [...itemsData].sort((a, b) => {
@@ -16,71 +16,73 @@ function ItemList() {
     }
   });
 
-  // Create Sort Buttons
-  const renderSortButtons = () => (
-    <div className="flex space-x-2 my-4">
-      <button
-        className={`${
-          sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-gray-300'
-        } px-4 py-2 rounded-md`}
-        onClick={() => setSortBy('name')}
-      >
-        Sort by Name
-      </button>
-      <button
-        className={`${
-          sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-gray-300'
-        } px-4 py-2 rounded-md`}
-        onClick={() => setSortBy('category')}
-      >
-        Sort by Category
-      </button>
-      <button
-        className={`${
-          grouped ? 'bg-blue-500 text-white' : 'bg-gray-300'
-        } px-4 py-2 rounded-md`}
-        onClick={() => setGrouped(!grouped)}
-      >
-        {grouped ? 'Ungroup' : 'Group by Category'}
-      </button>
-    </div>
-  );
-
-  // Group the items by category
-  const groupItemsByCategory = () => {
-    return sortedItems.reduce((groupedItems, currentItem) => {
-      const category = currentItem.category;
-      if (!groupedItems[category]) {
-        groupedItems[category] = [];
-      }
-      groupedItems[category].push(currentItem);
-      return groupedItems;
-    }, {});
+  // Function to toggle grouping
+  const toggleGrouped = () => {
+    setGrouped(!grouped);
   };
+
+  // Group the items by category and sort them alphabetically
+  const groupedItems = sortedItems.reduce((acc, item) => {
+    const category = item.category;
+    if (!acc[category]) {
+      acc[category] = [];
+    }
+    acc[category].push(item);
+    return acc;
+  }, {});
 
   // Render the Items
   const renderItems = () => {
     if (grouped) {
-      const groupedItems = groupItemsByCategory();
-      return Object.keys(groupedItems).map((category) => (
-        <div key={category}>
-          <h2 className="text-xl font-semibold mt-4 mb-2 capitalize">{category}</h2>
-          {groupedItems[category].map((item) => (
-            <Item key={item.id} item={item} />
-          ))}
-        </div>
-      ));
+      return Object.keys(groupedItems)
+        .sort()
+        .map((category) => (
+          <div key={category}>
+            <h2 className="text-xl font-semibold mt-4 mb-2 capitalize">{category}</h2>
+            {groupedItems[category].map((item) => (
+              <Item key={item.id} item={item} />
+            ))}
+          </div>
+        ));
     } else {
-      return sortedItems.map((item) => <Item key={item.id} item={item} />);
+      return sortedItems.map((item) => (
+        <Item key={item.id} item={item} />
+      ));
     }
   };
 
   return (
     <div>
-      {renderSortButtons()}
+      <div className="flex space-x-2 my-4">
+        <button
+          className={`${
+            sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-gray-300'
+          } px-4 py-2 rounded-md`}
+          onClick={() => setSortBy('name')}
+        >
+          Sort by Name
+        </button>
+        <button
+          className={`${
+            sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-gray-300'
+          } px-4 py-2 rounded-md`}
+          onClick={() => setSortBy('category')}
+        >
+          Sort by Category
+        </button>
+        <button
+          className={`${
+            grouped ? 'bg-blue-500 text-white' : 'bg-gray-300'
+          } px-4 py-2 rounded-md`}
+          onClick={toggleGrouped}
+        >
+          {grouped ? 'Ungroup' : 'Group by Category'}
+        </button>
+      </div>
+
       {renderItems()}
     </div>
   );
-}
+};
 
 export default ItemList;
